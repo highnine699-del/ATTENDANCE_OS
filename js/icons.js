23be189e235f372b9b7b359f94eb5790c98c20fe
Josiah <highnine699@gmail.com>
2026-07-07 22:46:15 +0100

    Fix missing backtick in icons.js cloud icon template literal

--- js/icons.js
+++ js/icons.js
@@ -56,7 +56,7 @@ export const icons = {
     progress: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="18" height="20" x="3" y="2" rx="2"/><path d="M7 2v20"/><path d="M17 2v20"/><path d="M12 2v20"/></svg>`,
     
     // Cloud
-    cloud: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17.5 19c0-1.7-1.3-3-3-3h-11c-1.7 0-3-1.3-3-3 0-1.7 1.3-3 3-3 0-1.4 1.1-2.5 2.5-2.5.4 0 .7.1 1 .2.8-1.6 2.5-2.7 4.5-2.7 2.8 0 5 2.2 5 5 1.9 0 3.5 1.6 3.5 3.5 0 1.9-1.6 3.5-3.5 3.5z"/></svg>,
+    cloud: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17.5 19c0-1.7-1.3-3-3-3h-11c-1.7 0-3-1.3-3-3 0-1.7 1.3-3 3-3 0-1.4 1.1-2.5 2.5-2.5.4 0 .7.1 1 .2.8-1.6 2.5-2.7 4.5-2.7 2.8 0 5 2.2 5 5 1.9 0 3.5 1.6 3.5 3.5 0 1.9-1.6 3.5-3.5 3.5z"/></svg>`,
 
     // Extension
     extension: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.5 6c-2.6.7-5.3 1-8 1s-5.4-.3-8-1"/><path d="M14.5 8c2.6.7 5.3 1 8 1s5.4-.3 8-1"/><path d="M12 2v20"/><path d="M4.5 9c2.6.7 5.3 1 8 1s5.4-.3 8-1"/></svg>`
